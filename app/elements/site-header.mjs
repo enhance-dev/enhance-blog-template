077a7df8ba7d6d44602df003b144932552dedfe3
Cole Peters <cole@colepeters.com>
2023-03-15 15:38:20 -0500

    Remove nav

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -1,15 +1,9 @@
 export default function SiteHeader ({ html }) {
   return html`
-    <h1 class='text2 text4-lg text-center font-heading font-bold tracking-2 pt4 pt6-lg pb0 pb2-lg'>Site Title</h1>
-    <nav class='pb4 pb6-lg'>
-      <ul class='text-center font-body uppercase tracking2 flex flex-row justify-center gap0 list-none'>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/blog'>Blog</a>
-        </li>
-      </ul>
-    </nav>
+    <h1 class='text2 text4-lg text-center font-heading font-bold tracking-2 pt4 pt6-lg pb4 pb6-lg'>
+      <a href='/' class='no-underline'>
+        Site Title
+      </a>
+    </h1>
   `
 }
