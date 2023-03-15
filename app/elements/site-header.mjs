c681183054c5a05bda81e22d221a0a96ecaffdd2
Cole Peters <cole@colepeters.com>
2023-03-15 15:48:06 -0500

    Update site header, pad page bottom

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -1,9 +1,12 @@
 export default function SiteHeader ({ html }) {
   return html`
-    <h1 class='text2 text4-lg text-center font-heading font-bold tracking-2 pt4 pt6-lg pb4 pb6-lg'>
+    <h1 class='text2 text4-lg text-center font-heading font-bold tracking-2 pt4 pt6-lg pb0 pb2-lg'>
       <a href='/' class='no-underline'>
-        Site Title
+        Blog Title
       </a>
     </h1>
+    <p class='text-center font-body pb4 pb6-lg'>
+      A subtitle for this blog
+    </p>
   `
 }
