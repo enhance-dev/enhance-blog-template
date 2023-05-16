935813d91433a9a164ea8f7016ee6fef1e4bc234
macdonst <simon.macdonald@gmail.com>
2023-05-16 11:58:35 -0700

    Add rel me in head
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/head.mjs
+++ app/head.mjs
@@ -2,6 +2,7 @@ import { getLinkTag } from '@enhance/arc-plugin-styles/get-styles'
 
 export default function Head() {
   const siteUrl = process.env.SITE_URL || 'http://localhost:3333'
+  const me = null // Ex. 'https://social.example.com/@username'
   return`
     <!DOCTYPE html>
     <html lang='en'>
@@ -12,6 +13,7 @@ export default function Head() {
         <link rel='stylesheet' href='/_public/css/global.css' />
         <link rel='stylesheet' href='/_public/css/a11y-dark.min.css' />
         <link rel="webmention" href="${siteUrl}/webmention">
+        ${me && `<link href="${me}" rel="me">`}
       </head>
   `
 }
