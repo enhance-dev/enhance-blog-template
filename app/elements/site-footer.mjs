1038d42f7df9dc61a6e53969c74cb1c93e81ed7a
macdonst <simon.macdonald@gmail.com>
2023-05-16 11:16:37 -0700

    Add optional rel me link to blog template
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/elements/site-footer.mjs
+++ app/elements/site-footer.mjs
@@ -1,6 +1,10 @@
 export default function SiteFooter ({ html }) {
+  const me = null // Ex. 'https://social.example.com/@username'
   return html`
     <footer class='pt4 pt6-lg pb4 pb6-lg'>
+      ${me && `<p class='text-center pb-4'>
+        <a rel='me' href='${me}' class='font-body text-1 uppercase tracking3 underline'>Mastodon</a>
+      </p>`}
       <p class='text-center'>
         <a href='/rss' class='font-body text-1 uppercase tracking3 underline'>RSS</a>
       </p>
